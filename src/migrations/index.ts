a59d6a8ee71e7d26efebec7ea035f075574f2a1b
omaro <omaroubari16@gmail.com>
2025-01-12 18:17:11 +0300

    add logos collection

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250112_151648 from './20250112_151648';
+
+export const migrations = [
+  {
+    up: migration_20250112_151648.up,
+    down: migration_20250112_151648.down,
+    name: '20250112_151648'
+  },
+];
