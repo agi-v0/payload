51995f373ed969a084edd6b06fe49d3764a9d52c
omaro <omaroubari16@gmail.com>
2025-01-13 22:22:42 +0300

    s3 plugin was causing seeding to fail, so I disabled it locally while i debug

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -220,7 +220,6 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
-  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -1098,7 +1097,6 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
-  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
