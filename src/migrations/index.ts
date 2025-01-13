455fd032e2a29a26a78bc6c575805fde3011cd0a
omaro <omaroubari16@gmail.com>
2025-01-14 00:21:12 +0300

    test workflow: add logos collection

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250113_200624 from './20250113_200624'
+import * as migration_20250113_200624 from './20250113_200624';
+import * as migration_20250113_212056 from './20250113_212056';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250113_200624.down,
     name: '20250113_200624',
   },
-]
+  {
+    up: migration_20250113_212056.up,
+    down: migration_20250113_212056.down,
+    name: '20250113_212056'
+  },
+];
