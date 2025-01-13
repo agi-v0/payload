ed09178c605a29581b2b593304af3fbbd6c44050
omaro <omaroubari16@gmail.com>
2025-01-13 23:46:34 +0300

    reset db schema local + prod

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250113_195842 from './20250113_195842';
+import * as migration_20250113_200624 from './20250113_200624'
 
 export const migrations = [
   {
-    up: migration_20250113_195842.up,
-    down: migration_20250113_195842.down,
-    name: '20250113_195842'
+    up: migration_20250113_200624.up,
+    down: migration_20250113_200624.down,
+    name: '20250113_200624',
   },
-];
+]
