e465280231fff992c452335cfa247b2257f1146e
omaro <omaroubari16@gmail.com>
2025-01-13 11:15:22 +0300

    x

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -63,7 +63,7 @@ export default buildConfig({
   editor: defaultLexical,
   db: vercelPostgresAdapter({
     pool: {
-      connectionString: process.env.POSTGRES_URL || '',
+      connectionString: process.env.DATABASE_URI || '',
     },
   }),
 
