722e751653947bcb733df26310171f4c0b923ef0
omaro <omaroubari16@gmail.com>
2025-01-13 22:27:39 +0300

    migration script

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250113_065100 from './20250113_065100'
+import * as migration_20250113_065100 from './20250113_065100';
+import * as migration_20250113_192657 from './20250113_192657';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250113_065100.down,
     name: '20250113_065100',
   },
-]
+  {
+    up: migration_20250113_192657.up,
+    down: migration_20250113_192657.down,
+    name: '20250113_192657'
+  },
+];
