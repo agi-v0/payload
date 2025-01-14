9bcbdaa246607932850c1bd74e99f417228ec1a6
omaro <omaroubari16@gmail.com>
2025-01-14 20:37:55 +0300

    x

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250114_172554 from './20250114_172554';
+import * as migration_20250114_173457 from './20250114_173457';
 
 export const migrations = [
   {
-    up: migration_20250114_172554.up,
-    down: migration_20250114_172554.down,
-    name: '20250114_172554'
+    up: migration_20250114_173457.up,
+    down: migration_20250114_173457.down,
+    name: '20250114_173457'
   },
 ];
