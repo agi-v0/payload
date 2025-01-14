de5e81a14c8b5d15fedeeb971649d2b5681a824a
omaro <omaroubari16@gmail.com>
2025-01-14 20:16:22 +0300

    reset migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250113_200624 from './20250113_200624'
+import * as migration_20250113_200624 from './20250113_200624';
+import * as migration_20250114_171410 from './20250114_171410';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250113_200624.down,
     name: '20250113_200624',
   },
-]
+  {
+    up: migration_20250114_171410.up,
+    down: migration_20250114_171410.down,
+    name: '20250114_171410'
+  },
+];
