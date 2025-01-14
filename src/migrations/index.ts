dd6bb108a6c9726dbd68732151c31a880260de31
omaro <omaroubari16@gmail.com>
2025-01-14 15:42:46 +0300

    create migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250113_200624 from './20250113_200624'
+import * as migration_20250113_200624 from './20250113_200624';
+import * as migration_20250114_124225 from './20250114_124225';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250113_200624.down,
     name: '20250113_200624',
   },
-]
+  {
+    up: migration_20250114_124225.up,
+    down: migration_20250114_124225.down,
+    name: '20250114_124225'
+  },
+];
