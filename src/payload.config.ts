f1951c42947a1a01c65bf0002b1513fa3397b7c6
omaro <omaroubari16@gmail.com>
2025-02-05 12:54:42 +0300

    add media prefix to s3 bucket

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -76,7 +76,9 @@ export default buildConfig({
     ...plugins,
     s3Storage({
       collections: {
-        media: true,
+        media: {
+          prefix: 'media',
+        },
       },
       bucket: process.env.S3_BUCKET || '',
       config: {
