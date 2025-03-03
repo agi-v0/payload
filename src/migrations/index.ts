a780e677411b308197165c7b387830afae16c42d
omaro <omaroubari16@gmail.com>
2025-03-03 09:53:33 +0300

    temporary schema migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250205_172222_initial from './20250205_172222_initial';
 import * as migration_20250205_211146_add_i18n from './20250205_211146_add_i18n';
+import * as migration_20250303_064940_hero_variants from './20250303_064940_hero_variants';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250205_211146_add_i18n.up,
     down: migration_20250205_211146_add_i18n.down,
-    name: '20250205_211146_add_i18n'
+    name: '20250205_211146_add_i18n',
+  },
+  {
+    up: migration_20250303_064940_hero_variants.up,
+    down: migration_20250303_064940_hero_variants.down,
+    name: '20250303_064940_hero_variants'
   },
 ];
