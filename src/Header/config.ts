631cf785184de606b7702e9eaa3c6e4685bd8d8d
omaro <omaroubari16@gmail.com>
2025-04-20 00:30:55 +0300

    feat(header-and-footer): customize schema

--- src/Header/config.ts
+++ src/Header/config.ts
@@ -1,34 +1,215 @@
 import type { GlobalConfig } from 'payload'
 
-import { link } from '@/fields/link'
-import { revalidateHeader } from './hooks/revalidateHeader'
+import { revalidatePath } from 'next/cache'
+
+import { authenticated } from '../access/authenticated'
+import { link } from '../fields/link'
 
 export const Header: GlobalConfig = {
   slug: 'header',
   access: {
     read: () => true,
+    update: authenticated,
   },
   fields: [
     {
-      name: 'navItems',
+      name: 'tabs',
       type: 'array',
-      localized: true,
-      fields: [
-        link({
-          colors: false,
-          variants: false,
-        }),
-      ],
-      maxRows: 6,
       admin: {
-        initCollapsed: true,
         components: {
-          RowLabel: '@/Header/RowLabel#RowLabel',
+          RowLabel: '@/Header/CustomRowLabelTabs',
         },
       },
+      fields: [
+        {
+          name: 'label',
+          type: 'text',
+          required: true,
+        },
+        {
+          type: 'row',
+          fields: [
+            {
+              name: 'enableDirectLink',
+              type: 'checkbox',
+            },
+            {
+              name: 'enableDropdown',
+              type: 'checkbox',
+            },
+          ],
+        },
+        {
+          type: 'collapsible',
+          admin: {
+            condition: (_, siblingData) => siblingData.enableDirectLink,
+          },
+          fields: [
+            link({
+              variants: false,
+              colors: false,
+              icon: false,
+              description: false,
+              disableLabel: true,
+            }),
+          ],
+          label: 'Direct Link',
+        },
+        {
+          type: 'collapsible',
+          admin: {
+            condition: (_, siblingData) => siblingData.enableDropdown,
+          },
+          fields: [
+            {
+              name: 'description',
+              type: 'textarea',
+            },
+            {
+              name: 'descriptionLinks',
+              type: 'array',
+              fields: [
+                link({
+                  variants: false,
+                  colors: false,
+                  icon: false,
+                  description: false,
+                  overrides: {
+                    label: false,
+                  },
+                }),
+              ],
+            },
+            {
+              name: 'navItems',
+              type: 'array',
+              admin: {
+                components: {
+                  RowLabel: '@/Header/CustomRowLabelNavItems',
+                },
+              },
+              fields: [
+                {
+                  name: 'style',
+                  type: 'select',
+                  defaultValue: 'default',
+                  options: [
+                    {
+                      label: 'Default',
+                      value: 'default',
+                    },
+                    {
+                      label: 'Featured',
+                      value: 'featured',
+                    },
+                    {
+                      label: 'List',
+                      value: 'list',
+                    },
+                  ],
+                },
+                {
+                  name: 'defaultLink',
+                  type: 'group',
+                  admin: {
+                    condition: (_, siblingData) => siblingData.style === 'default',
+                  },
+                  fields: [
+                    link({
+                      variants: false,
+                      colors: false,
+                      icon: false,
+                      description: false,
+                      overrides: {
+                        label: false,
+                      },
+                    }),
+                    {
+                      name: 'description',
+                      type: 'textarea',
+                    },
+                  ],
+                },
+                {
+                  name: 'featuredLink',
+                  type: 'group',
+                  admin: {
+                    condition: (_, siblingData) => siblingData.style === 'featured',
+                  },
+                  fields: [
+                    {
+                      name: 'tag',
+                      type: 'text',
+                    },
+                    {
+                      name: 'label',
+                      type: 'richText',
+                    },
+                    {
+                      name: 'links',
+                      type: 'array',
+                      fields: [
+                        link({
+                          variants: false,
+                          colors: false,
+                          icon: false,
+                          description: false,
+                          overrides: {
+                            label: false,
+                          },
+                        }),
+                      ],
+                    },
+                  ],
+                },
+                {
+                  name: 'listLinks',
+                  type: 'group',
+                  admin: {
+                    condition: (_, siblingData) => siblingData.style === 'list',
+                  },
+                  fields: [
+                    {
+                      name: 'tag',
+                      type: 'text',
+                    },
+                    {
+                      name: 'links',
+                      type: 'array',
+                      fields: [
+                        link({
+                          variants: false,
+                          colors: false,
+                          icon: false,
+                          description: false,
+                          overrides: {
+                            label: false,
+                          },
+                        }),
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+          label: 'Dropdown Menu',
+        },
+      ],
+      label: 'Main Menu Items',
     },
+    link({
+      variants: false,
+      colors: false,
+      icon: false,
+      description: false,
+      overrides: {
+        name: 'menuCta',
+        label: 'Menu CTA Button',
+      },
+    }),
   ],
   hooks: {
-    afterChange: [revalidateHeader],
+    afterChange: [() => revalidatePath('/', 'layout')],
   },
 }
