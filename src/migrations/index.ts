c2157047d2ce358d3dc3986312189268f1c53a39
omaro <omaroubari16@gmail.com>
2025-05-01 15:10:39 +0300

    feat(migrations): add new migration for pages and features schema updates
    - Introduced a new migration file to create and update various tables related to pages and features, including `pages_hero_links`, `pages_hero_logos`, and `pages_blocks_apps`.
    - Added a `reverse_order` column to the `pages_blocks_features_columns` table and modified the `app_icons` table to drop the `prefix` column.
    - Updated the migration index to include the new migration for proper execution order.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250430_175706_initial from './20250430_175706_initial';
+import * as migration_20250501_121019 from './20250501_121019';
 
 export const migrations = [
   {
     up: migration_20250430_175706_initial.up,
     down: migration_20250430_175706_initial.down,
-    name: '20250430_175706_initial'
+    name: '20250430_175706_initial',
+  },
+  {
+    up: migration_20250501_121019.up,
+    down: migration_20250501_121019.down,
+    name: '20250501_121019'
   },
 ];
