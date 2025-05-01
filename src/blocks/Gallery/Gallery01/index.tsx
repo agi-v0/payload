545277a504f569d663c6c660668c21ed48e53e46
omaro <omaroubari16@gmail.com>
2025-05-01 16:05:06 +0300

    feat(gallery): add 'use client' directive to Gallery components for client-side rendering

--- src/blocks/Gallery/Gallery01/index.tsx
+++ src/blocks/Gallery/Gallery01/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import type { GalleryBlock } from '@/payload-types'
 import { Media } from '@/components/Media'
--- src/blocks/Gallery/Gallery02/index.tsx
+++ src/blocks/Gallery/Gallery02/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import type { GalleryBlock } from '@/payload-types'
 import { Media } from '@/components/Media'
