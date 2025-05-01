ddb06fc35a584919877e268dde4745b782db5b17
omaro <omaroubari16@gmail.com>
2025-05-01 15:07:52 +0300

    fix(slug): update regex in formatSlug functions to allow slashes
    - Modified regex in formatSlug functions to permit slashes in slugs, enhancing slug formatting capabilities.

--- src/fields/slug/formatSlug.ts
+++ src/fields/slug/formatSlug.ts
@@ -3,7 +3,7 @@ import type { FieldHook } from 'payload'
 export const formatSlug = (val: string): string =>
   val
     .replace(/ /g, '-')
-    .replace(/[^\w-]+/g, '')
+    .replace(/[^\w-\/]+/g, '')
     .toLowerCase()
 
 export const formatSlugHook =
