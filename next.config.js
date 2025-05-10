5120cf24da1a03f0eb3f8af0391909fc7074c08c
omaro <omaroubari16@gmail.com>
2025-05-10 14:42:34 +0300

    ci(next-config): add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import createNextIntlPlugin from 'next-intl/plugin'
-
+import bundleAnalyzer from '@next/bundle-analyzer'
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
@@ -27,4 +27,8 @@ const nextConfig = {
   redirects,
 }
 
-export default withPayload(withNextIntl(nextConfig))
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+export default withBundleAnalyzer(withPayload(withNextIntl(nextConfig)))
